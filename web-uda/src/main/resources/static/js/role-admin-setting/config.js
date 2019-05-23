266bae47882b35c6c036b05789aa875d27335849
Roxas0309 <bimasebayang11@gmail.com>
2019-05-23 15:22:25 +0700

    commit untuk role detail config

--- web-uda/src/main/resources/static/js/role-admin-setting/config.js
+++ web-uda/src/main/resources/static/js/role-admin-setting/config.js
@@ -1,4 +1,25 @@
-masterWebUdaIndexApp.controller('user-admin-setting-config', function($scope,
+masterWebUdaIndexApp.controller('role-admin-setting-config', function($scope,
 		$http, $rootScope, $location, $window) {
+   
+    $scope.isActivePaging(false);
 
-}
+	$scope.navActivatorRAT = "active";
+	$scope.navActivatorRDAT = "";
+
+	$scope.navChange = function(arg1,arg2){
+		if(arg1=="active"){
+			$scope.navActivatorRAT = "";
+		}
+		else{
+			$scope.navActivatorRAT = "active";
+		}
+		
+		if(arg2=="active"){
+			$scope.navActivatorRDAT = "";
+		}
+		else{
+			$scope.navActivatorRDAT = "active";
+		}
+	}
+	
+});
